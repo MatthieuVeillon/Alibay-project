2a83b1c857e1d7297d6acc6609d68bc25ca6267e
Maxime Lussier <m.lussier@live.ca>
2017-11-09 05:59:38 -0500

    Display bought item

--- src/Components/jsx/AccountPage.jsx
+++ src/Components/jsx/AccountPage.jsx
@@ -10,7 +10,11 @@ import {
   FormControl,
 } from "react-bootstrap";
 import "../css/Account.css";
-import {allItemsSold, getItemDescription} from "../../backend-mockup";
+import {
+  allItemsSold,
+  getItemDescription,
+  allItemsBought,
+} from "../../backend-mockup";
 
 class AccountPage extends Component {
   getAllDescriptionSold = sellerId => {
@@ -54,17 +58,64 @@ class AccountPage extends Component {
 
     return htmlDescription;
   };
+  getAllDescriptionBought = sellerId => {
+    // an array of sold listing IDs
+    const ids = allItemsBought(sellerId);
+    // an array of sold listing full description
+
+    if (!Array.isArray(ids) || !ids.length) {
+      // check if array does not exist, or is not an array, or is empty
+      const noProductBought = "no object bought yet";
+
+      return noProductBought;
+    }
+
+    const descriptionsArray = ids.map(id => getItemDescription(id));
+
+    return descriptionsArray;
+  };
+  displayBought = sellerId => {
+    const descriptions = this.getAllDescriptionBought(sellerId);
+
+    if (typeof descriptions === "string") {
+      return (
+        <div>
+          <Jumbotron>
+            <h3>No object bought yet</h3>
+          </Jumbotron>
+        </div>
+      );
+    }
+    // iterate through array of descrpition object to populate an array of html elements
+    const htmlDescription = descriptions.map((desc, i) => (
+      <div key={i}>
+        <Jumbotron>
+          <h3>{desc.productName}</h3>
+          <h4>Price: {desc.price}</h4>
+          <h4>Description: {desc.blurb}</h4>
+        </Jumbotron>
+      </div>
+    ));
+
+    return htmlDescription;
+  };
   render() {
     return (
       <Grid className="componentHeader">
         <Row>
           <Col xs={12}>
             <h1>Hello id {this.props.currentUserId}</h1>
+          </Col>
+          <Col xs={6}>
             <h2>Products sold:</h2>
+          </Col>
+          <Col xs={6}>
+            <h2>Products bought:</h2>
           </Col>
         </Row>
         <Row>
           <Col xs={6}>{this.displaySold(this.props.currentUserId)}</Col>
+          <Col xs={6}>{this.displayBought(this.props.currentUserId)}</Col>
         </Row>
       </Grid>
     );
